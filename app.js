8130ea86980f627ff50eee9ab28e6d8011176363
James <jamesmcdaniel33@hotmail.com>
2023-06-21 09:10:23 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName = James;
+let lastName;
+var age;
 
 // Exercise 2
+firstName = "James";
+lastName = "McDaniel";
+age = 30;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let caseSensitive = true;
+
+//Exercise 4
+let price = 19.99;
+let onSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSaize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
